81ac3d814a45ce42035eabb82171b16226efcb54
JoseQuispeCahuana <lacarpetadelvergazo1@gmail.com>
2020-08-02 06:11:46 -0500

    Updates

---
+++ precache-manifest.2f5b6c374ffd71ab3c76b3f1d1ee5096.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4667b485d4a75aa2d08300c255246834",
+    "url": "/my-app-react5/index.html"
+  },
+  {
+    "revision": "be758ed21246726aa622",
+    "url": "/my-app-react5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba3d1a5fc7623aee92a7",
+    "url": "/my-app-react5/static/js/2.501be1cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react5/static/js/2.501be1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be758ed21246726aa622",
+    "url": "/my-app-react5/static/js/main.b454e9e1.chunk.js"
+  },
+  {
+    "revision": "769c5373623fa63ac71d",
+    "url": "/my-app-react5/static/js/runtime-main.b1fdb612.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react5/static/media/logo.5d5d9eef.svg"
+  }
+]);
